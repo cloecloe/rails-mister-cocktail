304715196de64751cac5fc67ea9ad67da73d2dfc
Cloe Furlan <cloe.furlan@yahoo.fr>
2020-08-13 17:13:06 -0400

    Search bar working

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,10 @@
 class CocktailsController < ApplicationController
   def index
-    @cocktails = Cocktail.all
+    if params[:search]
+      @cocktails = Cocktail.where(name: params[:search])
+    else
+      @cocktails = Cocktail.all
+    end
   end
 
   def show
